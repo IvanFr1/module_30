d93d48595fd9eaef4de4d2d3ad58bce4f8d548d2
IvanFr1 <128962985+IvanFr1@users.noreply.github.com>
2025-06-08 21:47:29 +0300

    Update conftest.py

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -4,10 +4,11 @@ from fastapi.testclient import TestClient
 from app.database import engine
 from app.main import app
 from app.models import Base
-
+from typing import AsyncGenerator
+from httpx import AsyncClient
 
 @pytest.fixture(autouse=True)
-async def prepare_database():
+async def prepare_database() -> AsyncGenerator[AsyncClient, None]:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
@@ -16,6 +17,6 @@ async def prepare_database():
 
 
 @pytest.fixture
-def client():
+def client() -> AsyncGenerator[AsyncClient, None]:
     with TestClient(app) as client:
         yield client
