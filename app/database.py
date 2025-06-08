4c97b2b2883c2469f1b75a519b8165b9d619ed1c
IvanFr1 <128962985+IvanFr1@users.noreply.github.com>
2025-06-08 19:41:47 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,17 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-AsyncSessionLocal = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, expire_on_commit=False
 )
 
 Base = declarative_base()
-
 
 async def get_db():
     async with AsyncSessionLocal() as db:
