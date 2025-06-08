867ef09105d123d252fd7f9e3208f9a599c8586d
IvanFr1 <128962985+IvanFr1@users.noreply.github.com>
2025-06-08 21:50:42 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from typing import AsyncIterator, List
-
+from typing import Any
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
